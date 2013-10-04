6572b0ad5c44f4cf4b8b807382203f0ad8be0987
Enrique <engapa@gmail.com>
2013-10-04 12:17:14 +0200

    Use env variables fo openshift

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 /**
- * Server side 
- **/
+* Server 
+**/
 
-var vertx = require('vertx');
+load('vertx.js');
+
+//var vertx = require('vertx');
+var port = parseInt(vertx.env['OPENSHIFT_INTERNAL_PORT']);
+var host = vertx.env['OPENSHIFT_INTERNAL_IP'];
 
 vertx.createHttpServer().requestHandler(function(req) {
   req.response.end("Hello World!");
-}).listen(8080, 'localhost');
+}).listen(port, host);
