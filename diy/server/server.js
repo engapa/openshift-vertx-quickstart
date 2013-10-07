1b6b8b95be857fd8219eee989d8b16054a5a07e5
Enrique <engapa@gmail.com>
2013-10-07 10:24:39 +0200

    Console messages

--- diy/server/server.js
+++ diy/server/server.js
@@ -2,12 +2,20 @@
 * Server 
 **/
 
-load('vertx.js');
+var vertx = require('vertx');
+var container = require('vertx/container');
+var console = require('vertx/console');
 
-//var vertx = require('vertx');
-var port = parseInt(vertx.env['OPENSHIFT_DIY_PORT']);
-var host = vertx.env['OPENSHIFT_DIY_IP'];
+console.log("Loading env variables ...");
 
+var port = parseInt(container.env['OPENSHIFT_DIY_PORT']);
+var host = container.env['OPENSHIFT_DIY_IP'];
+
+console.log("OPENSHIFT_DIY_PORT: " + port);
+console.log("OPENSHIFT_DIY_IP: " + host);
+
+
+console.log("Starting server ...");
 vertx.createHttpServer().requestHandler(function(req) {
   req.response.end("Hello World!");
 }).listen(port, host);
